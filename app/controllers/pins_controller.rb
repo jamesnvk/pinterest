016219579ef16c04976b0622cae70b9e7c30ce5d
James Novak <jamesnovak90@gmail.com>
2016-09-01 21:54:42 -0400

    fixed up code

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -26,30 +26,26 @@ class PinsController < ApplicationController
     @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.' 
-      
     else
       render action: 'new' 
-      
     end
-end
+  end
 
  
   def update
-  
     if @pin.update(pin_params)
        redirect_to @pin, notice: 'Pin was successfully updated.' 
     else
        render :edit 
     end
-end
+  end
 
   def destroy
-  @pin.destroy
-  redirect_to pins_url, notice: 'Pin was successfully destroyed.' 
-end
+    @pin.destroy
+    redirect_to pins_url, notice: 'Pin was successfully destroyed.' 
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
@@ -59,7 +55,6 @@ end
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
