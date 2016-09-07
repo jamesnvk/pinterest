866c733c56fa557391e657eccd37ad058e856bf2
James Novak <jamesnovak90@gmail.com>
2016-09-07 14:06:03 -0400

    added handlebars template to show page

--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,3 +1,4 @@
 class PinSerializer < ActiveModel::Serializer
   attributes :id, :description, :user_id, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+  has_one :user
 end
