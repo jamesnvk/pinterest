fc406b3f52794f4f121359bb8cab8ed1569625f2
James Novak <jamesnovak90@gmail.com>
2016-09-06 23:14:09 -0400

    add pin constructor

--- app/assets/javascripts/pin.js
+++ app/assets/javascripts/pin.js
@@ -3,7 +3,19 @@ $(function(){
     e.preventDefault
    var nextId = parseInt($('.js-next').attr('data-id')) + 1
    $.get("/pins/" + nextId + ".json", function(data) {
-    debugger;
+    var pin = new Pin(data)
     });
   })
 })
+
+
+function Pin(attributes) {
+  this.description = attributes.description
+  this.userId = attributes.user_id
+  this.id = attributes.id
+  this.imageFileName = attributes.image_file_name
+  this.imageContentType = attributes.image_content_type
+  this.imageFileSize = attributes.image_file_size
+  this.imageUpdatedAt = attributes.image_updated_at
+}
+
--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,3 +1,3 @@
 class PinSerializer < ActiveModel::Serializer
-  attributes :id, :description, :user_id, :image_file_name, :image_content_type, :image_file_size
+  attributes :id, :description, :user_id, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 end
