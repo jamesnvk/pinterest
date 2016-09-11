f3e5a99a0b0ced680952ec79f784f910509e993f
James Novak <jamesnovak90@gmail.com>
2016-09-11 13:28:58 -0400

    added comment model to database with associations, added routes, added feedback page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def feedback
+    @comment = Comment.new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
    has_many :pins
+   has_many :comments
 
    validates :name, presence: true
 end
