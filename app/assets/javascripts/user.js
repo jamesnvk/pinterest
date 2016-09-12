47be1f09da5db2748febd98d917fe212aa5d128b
James Novak <jamesnovak90@gmail.com>
2016-09-12 19:08:46 -0400

    working load comments, needs formatting

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,14 @@
+// load comments
 $(function(){
   $('#load-more').on('click', function(){
-    alert("hi")
+    var userId = parseInt($('#load-more').attr('data-id'))
+    $.get("/users/" + userId, function(data){
+      $('#load-more').remove()
+        $.each(data.comments, function(i, item){
+          $('.data').append(item.content)
+        })
+    })
   })
 })
+
+
