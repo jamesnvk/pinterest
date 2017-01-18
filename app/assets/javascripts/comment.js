6362829667403576f7b2d39ec35032efba8d349c
James Novak <jamesnovak90@gmail.com>
2017-01-18 16:34:32 -0500

    refactoring. wrapping comments and pins in iffes

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,40 +1,43 @@
-function Comment(attributes) {
-  this.content= attributes.content
-  this.userId = attributes.user_id
-  this.id = attributes.id
-}
+(function(){
 
-Comment.success = function(json){
- // debugger
-  var comment = new Comment(json)
-  var commentDisplay = comment.renderDisplay()
-  $('#feedbackResults').append(commentDisplay)
-  $('#comment_content').val('')
-}
+  function Comment(attributes) {
+    this.content= attributes.content
+    this.userId = attributes.user_id
+    this.id = attributes.id
+  }
 
-Comment.newComment = function(){
-  $('.new_comment').on('submit', function(e){
-    e.preventDefault()
-    var $form = $(this)
-    var action = $form.attr("action")
-    var params = $form.serialize()
+  Comment.success = function(json){
+   // debugger
+    var comment = new Comment(json)
+    var commentDisplay = comment.renderDisplay()
+    $('#feedbackResults').append(commentDisplay)
+    $('#comment_content').val('')
+  }
 
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
-    }).success(Comment.success)
+  Comment.newComment = function(){
+    $('.new_comment').on('submit', function(e){
+      e.preventDefault()
+      var $form = $(this)
+      var action = $form.attr("action")
+      var params = $form.serialize()
+
+      $.ajax({
+        url: action,
+        data: params,
+        dataType: "json",
+        method: "POST"
+      }).success(Comment.success)
+    })
+  }
+
+  Comment.prototype.renderDisplay = function() {
+    return Comment.template(this) // builds the HTML string json and returns the object (comment) itself
+  }
+
+  $(function(){
+    Comment.templateSource = $("#comment-template").html() // grabs template from page
+    Comment.template = Handlebars.compile(Comment.templateSource) // Handlebars compiles the given template
+    Comment.newComment()
   })
-}
 
-Comment.prototype.renderDisplay = function() {
-  return Comment.template(this) // builds the HTML string json and returns the object (comment) itself
-}
-
-$(function(){
-  Comment.templateSource = $("#comment-template").html() // grabs template from page
-  Comment.template = Handlebars.compile(Comment.templateSource) // Handlebars compiles the given template
-  Comment.newComment()
-})
-
+})();
--- app/assets/javascripts/pin.js
+++ app/assets/javascripts/pin.js
@@ -1,55 +1,57 @@
-function Pin(attributes) {
-  this.description = attributes.description
-  this.userId = attributes.user_id
-  this.id = attributes.id
-  this.imageFileName = attributes.image_file_name
-  this.imageContentType = attributes.image_content_type
-  this.imageFileSize = attributes.image_file_size
-  this.imageUpdatedAt = attributes.image_updated_at
-  this.username = attributes.user.name
-}
+(function(){
 
-Pin.prototype.renderDisplay = function() {
-  return Pin.template(this) // builds the HTML string json and returns the object (pin) itself
-}
+  function Pin(attributes) {
+    this.description = attributes.description
+    this.userId = attributes.user_id
+    this.id = attributes.id
+    this.imageFileName = attributes.image_file_name
+    this.imageContentType = attributes.image_content_type
+    this.imageFileSize = attributes.image_file_size
+    this.imageUpdatedAt = attributes.image_updated_at
+    this.username = attributes.user.name
+  }
 
-Pin.nextPage = function(){
-  $('.js-next').on('click', function(e){
-    e.preventDefault
-    var nextId = parseInt($('.js-next').attr('data-id')) + 1
-      $.get("/pins/" + nextId + ".json", function(data) {
-        var pin = new Pin(data)
-        var pinDisplay = pin.renderDisplay()
-          $('#pinResults').html(pinDisplay)
-          $('.js-next').attr('data-id', data['id'])
-    });
-  })
-}
+  Pin.prototype.renderDisplay = function() {
+    return Pin.template(this) // builds the HTML string json and returns the object (pin) itself
+  }
 
-Pin.destroy = function(){
-  $('.deletePin').on('click', function(e){
-
-    var action = $(this).attr('href')
-    //debugger
-    $.ajax({
-      url: action,
-      dataType: "json",
-      method: "DELETE"
-    }).success(function(){
-      
-    }).error(function(error){
-      debugger
-      alert("did not succeed")
+  Pin.nextPage = function(){
+    $('.js-next').on('click', function(e){
+      e.preventDefault
+      var nextId = parseInt($('.js-next').attr('data-id')) + 1
+        $.get("/pins/" + nextId + ".json", function(data) {
+          var pin = new Pin(data)
+          var pinDisplay = pin.renderDisplay()
+            $('#pinResults').html(pinDisplay)
+            $('.js-next').attr('data-id', data['id'])
+      });
     })
-  return false
+  }
+
+  Pin.destroy = function(){
+    $('.deletePin').on('click', function(e){
+
+      var action = $(this).attr('href')
+      //debugger
+      $.ajax({
+        url: action,
+        dataType: "json",
+        method: "DELETE"
+      }).success(function(){
+        
+      }).error(function(error){
+        debugger
+        alert("did not succeed")
+      })
+    return false
+    })
+  }
+
+  $(function(){
+    Pin.templateSource = $("#pin-template").html() // grabs source of template
+    Pin.template = Handlebars.compile(Pin.templateSource) // Handlebars compiles the given template
+    Pin.nextPage()
+    Pin.destroy()
   })
-}
 
-
-
-$(function(){
-  Pin.templateSource = $("#pin-template").html() // grabs source of template
-  Pin.template = Handlebars.compile(Pin.templateSource) // Handlebars compiles the given template
-  Pin.nextPage()
-  Pin.destroy()
-})
+})();
