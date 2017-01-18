9e2f5694985e610ad6f772eb9d75537e92cb721a
James Novak <jamesnovak90@gmail.com>
2017-01-18 16:45:03 -0500

    add json response for all pins from index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,6 +6,10 @@ class PinsController < ApplicationController
 
   def index
     @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @pins}
+    end
   end
 
 
